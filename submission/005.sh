4e0a36d6c985a36ee3fc1c30a7bec2757cc60c8b
Edil Medeiros <jose.edil@gmail.com>
2024-10-16 15:04:31 -0300

    Solve all

--- submission/005.sh
+++ submission/005.sh
@@ -1,2 +1,31 @@
+#!/bin/bash
+
 # Create a 1-of-4 P2SH multisig address from the public keys in the four inputs of this tx:
 #   `37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517`
+
+BITCOIN="bitcoin-cli"
+
+txid=37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517
+
+# Get transaction data
+rawtransaction=$($BITCOIN getrawtransaction $txid)
+decodedtransaction=$($BITCOIN decoderawtransaction $rawtransaction)
+txins=$(echo $decodedtransaction | jq -cr '.vin[]')
+
+# Manual inspection shows it consists of 4 p2wpkh inputs. Thus, second witness
+# field must be the pubkeys.
+pubkeys=$(echo $txins | jq -c '.txinwitness[1]')
+
+# Build the argument for bitcoin-cli
+ks="["
+for key in ${pubkeys[@]}
+do
+    ks+=$key","
+done
+ks=${ks%","}"]"  # Remove trailing ',' and add closing ']'
+
+# Now build the multisig address
+multisig=$($BITCOIN createmultisig 1 "$ks")
+multisigaddress=$(echo $multisig | jq -cr '.address')
+
+echo $multisigaddress
--- submission/007.sh
+++ submission/007.sh
@@ -1,1 +1,35 @@
+#!/bin/bash
+
 # Only one single output remains unspent from block 123,321. What address was it sent to?
+
+BITCOIN="bitcoin-cli"
+
+# Get transaction ids from block 123321
+blockhash=$($BITCOIN getblockhash 123321)
+blockdata=$($BITCOIN getblock $blockhash)
+transactions=$(echo $blockdata | jq -c -r '.tx[]')
+
+# For each transaction...
+for tx in ${transactions[@]}
+do
+    # Get outputs
+    raw=$($BITCOIN getrawtransaction $tx)
+    decoded=$($BITCOIN decoderawtransaction $raw)
+    vouts=$(echo $decoded | jq -c -r '.vout[].n')
+
+    # For each vout, check if it is spent or not
+    for vout in ${vouts[@]}
+    do
+        # Inspect transaction: empty if transaction is spent
+        filter=$($BITCOIN gettxout $tx $vout)
+
+        # Found an unspent transaction: echo it and stop
+        if [[ ! -z $filter ]]
+        then
+            address=$(echo $filter | jq -c -r '.scriptPubKey.address')
+            echo $address
+            break
+        fi
+
+    done
+done
